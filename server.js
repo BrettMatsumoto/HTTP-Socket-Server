7f24299b74ece3edb4ffade9577ea0d7969ba65a
Brett <brett_matsumoto@yahoo.com>
2019-04-18 14:24:12 -1000

    added date to header

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const styles = require('./styles.js');
 const helium = require('./helium.js');
 const hydrogen = require('./hydrogen.js');
 const index = require('./index.js');
-
+const getNow = new Date;
+const date = getNow.toUTCString()
+console.log(date)
 let response = '';
 
 // this creates a server
@@ -27,26 +29,31 @@ Content-Length: ${helium.content.length}
 ${helium.content}`;
       } else if (headRequest == 'hydrogen.html') {
         response = `HTTP/1.1 200 OK
+Date: ${date}
 Content-Length: ${hydrogen.content.length}
 
 ${hydrogen.content}`;
       } else if (headRequest == 'styles.html') {
         response = `HTTP/1.1 200 OK
+Date: ${date}
 Content-Length: ${styles.content.length}
 
 ${styles.content}`;
       } else if (headRequest == '404.html') {
         response = `HTTP/1.1 200 OK
+Date: ${date}
 Content-Length: ${send404.content.length}
 
 ${send404.content}`;
       } else if (headRequest = 'index.html') {
         response = `HTTP/1.1 200 OK
+Date: ${date}
 Content-Length: ${index.content.length}
 
 ${index.content}`
       } else {
         response = `HTTP/1.1 200 OK
+Date: ${date}
 Content-Length: 18
 
 Welcome to nowhere`;
