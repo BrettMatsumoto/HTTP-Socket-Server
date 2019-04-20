4c3a5182d3118aeaa6e5f5f927b36dc17d58bce5
Brett <brett_matsumoto@yahoo.com>
2019-04-19 15:48:44 -1000

    can talk to port 80 websitess

--- client.js
+++ client.js
@@ -1,57 +1,83 @@
 'use strict';
 
 const net = require('net');
-let port = 8080;
 let processArgs = process.argv;
 let date = new Date();
 let method = 'GET';
 
-// console.log('processArgument index 2:', processArgs[2]);
-
 if (!processArgs[2]) {
-  process.stdout.write('No URI'); //because processArgs[2] is the node command that the user is looking for so if there's no node command cannot search for anything, give them an error
+  process.stdout.write(`
+No URI given
+********************************************************************
+To use this client, please input "node client.js ['insert URI Here']
+Sample valid URI's:
+        *****************
+        * index.html    *
+        * styles.html   *
+        * helium.html   *
+        * espn.com      *
+        *****************
+`);
   process.exit();
 }
 
-let findURI = processArgs[2];
-let hostName = 'localhost';
+let argument = processArgs[2];
 
-let clientRequest = `${method} /${findURI} HTTP/1.1 
+if (!argument.includes('.com')) {
+  let hostName = 'localhost';
+  let port = 8080;
+  let clientRequest = `${method} /${argument} HTTP/1.1 
+  Date: ${date}
+  Host: ${hostName}
+  Accept: text/html, application/json
+  User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36
+  Connection: Keep-Alive
+  Content-Length: 11
+  
+  response: Hello World
+  `;
+
+  const client = net.createConnection(port, hostName);
+
+  client.on('connect', function() {
+    client.write(clientRequest);
+    console.log('connected to the server');
+  });
+
+  client.on('data', function(data) {
+    process.stdout.write(data);
+    client.end(function() {
+      console.log('leaving the server');
+    });
+  });
+} else {
+  let URIdotComIndex = argument.indexOf('.com' || '.net' || '.org' || '.gov');
+  let findHost = argument.substring(0, URIdotComIndex + 4);
+  let findURI = argument.substring(URIdotComIndex + 4, argument.length)
+  let port = 80;
+  let clientRequest = 
+`${method} /${findURI} HTTP/1.1 
 Date: ${date}
-Host: ${hostName}
+Host: ${findHost}
 Accept: text/html, application/json
 User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36
 Connection: Keep-Alive
-Content-Length: 11
 
-response: Hello World
 `;
-// console.log(clientRequest);
 
-const client = net.createConnection(port, hostName);
+  const client = net.createConnection(port, findHost);
 
-client.on('connect', function(){
-  client.write(clientRequest)
-  console.log(clientRequest)
+  client.on('connect', function(){
+    client.write(clientRequest);
+    console.log(clientRequest)
+    console.log('connected to the server');
+  });
 
-  console.log('connected to the server');
-})
-client.on('data', function(data){
-  process.stdout.write(data)
-})
-// client.on('end', function(){
-//   console.log('Goodbye Mr.17')
-// })
-// const client = net.createConnection(port, () => {
-//   client.on('connect', function(){
-//     client.write(clientRequest);
-//   })
-// })
-// client.on('data', (data) => {
-
-//   process.stdout.write(data);
-//   console.log('mycomment',process.stdout);
-// });
-// client.on('end', () => {
-//   console.log('bye come back later');
-// });
+  client.on('data', function(data){
+    process.stdout.write(data);
+    console.log(data);
+    client.end(function(){
+      console.log('leaving the server')
+    })
+  })
+}
